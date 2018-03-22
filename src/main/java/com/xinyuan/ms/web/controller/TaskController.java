7195de5787fe6614b8b0e76139e6c16958631d9c
马睿隆 <774328384@qq.com>
2018-03-22 11:51:21 +0800

    Revert "修改"
    This reverts commit e8cfd6e

---
+++ src/main/java/com/xinyuan/ms/web/controller/TaskController.java
@@ -0,0 +1,17 @@
+package com.xinyuan.ms.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author liang
+ */
+@RestController
+@RequestMapping("/task")
+public class TaskController {
+
+    public TaskDTO
+
+
+}
