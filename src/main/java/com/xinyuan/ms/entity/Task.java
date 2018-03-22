e8cfd6e75050288716551df56192aa048ba02d57
马睿隆 <774328384@qq.com>
2018-03-22 11:32:36 +0800

    修改

--- src/main/java/com/xinyuan/ms/entity/Task.java
+++ src/main/java/com/xinyuan/ms/entity/Task.java
@@ -1,13 +1,20 @@
 package com.xinyuan.ms.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Date;
 
+/**
+ * @author liang
+ */
 @Data
+@Entity
 @Table(name = "task")
 public class Task {
 
@@ -21,5 +28,24 @@ public class Task {
     @Column(name = "content")
     private String content;
 
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "file")
+    private String file;
+
+    @Column(name = "refer_id")
+    private int referId;
+
+    @Column(name = "create_time")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
+
+    @Column(name = "update_time")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date updateTime;
+
+    @Column(name = "version")
+    private int version;
 
 }
