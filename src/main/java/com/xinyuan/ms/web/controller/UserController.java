97c1655e43d2b1560c3afac70a2d5360475facc0
马睿隆 <774328384@qq.com>
2018-10-17 10:39:16 +0800

    111

--- src/main/java/com/xinyuan/ms/web/controller/UserController.java
+++ src/main/java/com/xinyuan/ms/web/controller/UserController.java
@@ -1,67 +1,62 @@
-package com.xinyuan.ms.web.controller;
-
-import com.xinyuan.ms.common.util.MD5Util;
-import com.xinyuan.ms.common.util.ResultUtil;
-import com.xinyuan.ms.common.web.Message;
-import com.xinyuan.ms.common.web.PageBody;
-import com.xinyuan.ms.entity.User;
-import com.xinyuan.ms.service.UserService;
-import com.xinyuan.ms.web.request.BoundWXRequest;
-import com.xinyuan.ms.web.request.SaveUserRequest;
-import com.xinyuan.ms.web.vo.UserShowVo;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-/**
- * @Author: hwz
- * @Date: Created in 15:15 2018/4/10.
- */
-@Api(description = "用户管理")
-@RestController
-@RequestMapping("/backstage/user")
-public class UserController {
-
-    @Autowired
-    private UserService userService;
-
-
-    @ApiOperation(value = "微信绑定接口", notes = "绑定微信")
-    @RequestMapping(value = "boundWX", method = RequestMethod.POST)
-    public ResponseEntity<Message> boundWX(@RequestBody BoundWXRequest boundWXRequest) {
-        Message message = null;
-        try {
-            message = userService.boundWX(boundWXRequest);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return ResponseEntity.ok(message);
-    }
-
-
-
-    @ApiOperation(value = "重置密码", notes = "重置密码")
-    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "List")
-    @RequestMapping(value = "resetPassword", method = RequestMethod.POST)
-    public ResponseEntity<Message> resetPassword(@RequestBody List<Long> id) {
-        Message message = ResultUtil.success("重置密码成功");
-        try {
-            for (long i : id) {
-                User user = userService.findById(i);
-                user.setPassword(MD5Util.MD5("password").toLowerCase());
-                userService.updateUser(user);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            message = ResultUtil.error(2003, "重置密码失败");
-        }
-        return ResponseEntity.ok(message);
-    }
-}
+//package com.xinyuan.ms.web.controller;
+//
+//import com.xinyuan.ms.common.util.MD5Util;
+//import com.xinyuan.ms.common.util.ResultUtil;
+//import com.xinyuan.ms.common.web.Message;
+//import com.xinyuan.ms.entity.User;
+//import com.xinyuan.ms.web.request.BoundWXRequest;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiImplicitParam;
+//import io.swagger.annotations.ApiOperation;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+///**
+// * @Author: hwz
+// * @Date: Created in 15:15 2018/4/10.
+// */
+//@Api(description = "用户管理")
+//@RestController
+//@RequestMapping("/backstage/user")
+//public class UserController {
+//
+//    @Autowired
+//    private UserService userService;
+//
+//
+//    @ApiOperation(value = "微信绑定接口", notes = "绑定微信")
+//    @RequestMapping(value = "boundWX", method = RequestMethod.POST)
+//    public ResponseEntity<Message> boundWX(@RequestBody BoundWXRequest boundWXRequest) {
+//        Message message = null;
+//        try {
+//            message = userService.boundWX(boundWXRequest);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return ResponseEntity.ok(message);
+//    }
+//
+//
+//
+//    @ApiOperation(value = "重置密码", notes = "重置密码")
+//    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "List")
+//    @RequestMapping(value = "resetPassword", method = RequestMethod.POST)
+//    public ResponseEntity<Message> resetPassword(@RequestBody List<Long> id) {
+//        Message message = ResultUtil.success("重置密码成功");
+//        try {
+//            for (long i : id) {
+//                User user = userService.findById(i);
+//                user.setPassword(MD5Util.MD5("password").toLowerCase());
+//                userService.updateUser(user);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            message = ResultUtil.error(2003, "重置密码失败");
+//        }
+//        return ResponseEntity.ok(message);
+//    }
+//}
--- src/main/java/com/xinyuan/ms/web/vo/UserVo.java
+++ src/main/java/com/xinyuan/ms/web/vo/UserVo.java
@@ -27,49 +27,4 @@ public class UserVo  {
     @Column(name = "password")
     private String password;
 
-    @ApiModelProperty(value = "头像", name = "icon", example = "/upload/afd5c1ee-409e-43b2-86b4-b239e0e91fd6.jpg")
-    private String icon;
-
-    @ApiModelProperty(value = "简介", name = "introduction", example = "简介")
-    private String introduction;
-
-    @ApiModelProperty(value = "性别，1男2女", name = "sex", example = "1")
-    private Integer sex;
-
-    @ApiModelProperty(value = "年龄", name = "age", example = "18")
-    private Integer age;
-
-    @ApiModelProperty(value = "收藏话题数量", name = "collectTopicNum", example = "0")
-    private Integer collectTopicNum;
-
-    @ApiModelProperty(value = "我关注的人数量", name = "concertUserNum", example = "0")
-    private Integer concertUserNum;
-
-    @ApiModelProperty(value = "关注我的人数量", name = "concertedUserNum", example = "0")
-    private Integer concertedUserNum;
-
-    @ApiModelProperty(value = "我收到的赞数量", name = "likedNum", example = "0")
-    private Integer likedNum;
-
-    @ApiModelProperty(value = "我被收藏的数量", name = "collectedNum", example = "0")
-    private Integer collectedNum;
-
-    @ApiModelProperty(value = "登录次数", name = "login_count", example = "0")
-    private Integer loginCount;
-
-    @ApiModelProperty(value = "状态(0,1)，0停用，1启用", name = "status", example = "0")
-    private Integer status;
-
-    @ApiModelProperty(value = "最后登录时间", name = "loginTime", example = "2018-04-12 10:55:23")
-    private Date loginTime;
-
-    @ApiModelProperty(value = "注册时间", name = "createTime", example = "2018-04-12 10:55:23")
-    private Date createTime;
-
-    @ApiModelProperty(value = "会员等级（1.普通会员，2高级会员，3顶级会员）", name = "vip", example = "1")
-    private Integer vip;
-
-    @ApiModelProperty(value = "消费金额", name = "money", example = "0")
-    private Double money ;
-
 }
