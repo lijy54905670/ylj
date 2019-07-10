d76823fa9bee24c0cf39850834bf51826965716d
hzx <23322083@qq.com>
2019-07-10 10:25:47 +0800

    1

--- src/main/java/com/xinyuan/ms/web/controller/UserController.java
+++ src/main/java/com/xinyuan/ms/web/controller/UserController.java
@@ -1,62 +1,65 @@
-//package com.xinyuan.ms.web.controller;
-//
-//import com.xinyuan.ms.common.util.MD5Util;
-//import com.xinyuan.ms.common.util.ResultUtil;
-//import com.xinyuan.ms.common.web.Message;
-//import com.xinyuan.ms.entity.User;
-//import com.xinyuan.ms.web.request.BoundWXRequest;
-//import io.swagger.annotations.Api;
-//import io.swagger.annotations.ApiImplicitParam;
-//import io.swagger.annotations.ApiOperation;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-///**
-// * @Author: hwz
-// * @Date: Created in 15:15 2018/4/10.
-// */
-//@Api(description = "用户管理")
-//@RestController
-//@RequestMapping("/backstage/user")
-//public class UserController {
-//
-//    @Autowired
-//    private UserService userService;
-//
-//
-//    @ApiOperation(value = "微信绑定接口", notes = "绑定微信")
-//    @RequestMapping(value = "boundWX", method = RequestMethod.POST)
-//    public ResponseEntity<Message> boundWX(@RequestBody BoundWXRequest boundWXRequest) {
-//        Message message = null;
-//        try {
-//            message = userService.boundWX(boundWXRequest);
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return ResponseEntity.ok(message);
-//    }
-//
-//
-//
-//    @ApiOperation(value = "重置密码", notes = "重置密码")
-//    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "List")
-//    @RequestMapping(value = "resetPassword", method = RequestMethod.POST)
-//    public ResponseEntity<Message> resetPassword(@RequestBody List<Long> id) {
-//        Message message = ResultUtil.success("重置密码成功");
-//        try {
-//            for (long i : id) {
-//                User user = userService.findById(i);
-//                user.setPassword(MD5Util.MD5("password").toLowerCase());
-//                userService.updateUser(user);
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            message = ResultUtil.error(2003, "重置密码失败");
-//        }
-//        return ResponseEntity.ok(message);
-//    }
-//}
+package com.xinyuan.ms.web.controller;
+
+import com.xinyuan.ms.common.util.ResultUtil;
+import com.xinyuan.ms.common.web.Message;
+import com.xinyuan.ms.common.web.PageBody;
+import com.xinyuan.ms.entity.User;
+import com.xinyuan.ms.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @Author: hzx
+ */
+@Api(description = "用户管理")
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @ApiOperation(value = "保存", notes = "保存")
+    @RequestMapping(value = "save", method = RequestMethod.POST)
+    public ResponseEntity<Message> save(@RequestBody User user) {
+        userService.save(user);
+        return ResponseEntity.ok(ResultUtil.success());
+    }
+
+    @ApiOperation(value = "删除", notes = "删除")
+    @RequestMapping(value = "delete", method = RequestMethod.POST)
+    public ResponseEntity<Message> delete(@RequestBody List<Long> ids) {
+        userService.removeList(ids);
+        return ResponseEntity.ok(ResultUtil.success());
+    }
+
+    @ApiOperation(value = "更新", notes = "更新")
+    @RequestMapping(value = "update", method = RequestMethod.POST)
+    public ResponseEntity<Message> update(@RequestBody User user) {
+        userService.update(user);
+        return ResponseEntity.ok(ResultUtil.success());
+    }
+
+
+    @ApiOperation(value = "条件查询", notes = "条件查询")
+    @RequestMapping(value = "query", method = RequestMethod.POST)
+    public ResponseEntity<Page<User>> query(@RequestBody PageBody pageBody) {
+        Page<User> page = null;
+        try {
+            page = userService.query(pageBody);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.ok(page);
+    }
+}
